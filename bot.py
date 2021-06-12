d1883591b829f406b3606185493a227f4f2f6501
FrostiiWeeb <rayanhassansuja@gmail.com>
2021-06-12 20:37:09 +0200

    Hi

---
+++ bot.py
@@ -0,0 +1,68 @@
+import discord
+from discord.ext import commands
+import asyncpg, asyncio
+import discord
+from discord.ext import commands
+import datetime
+import asyncio
+import os
+os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
+os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
+os.environ["JISHAKU_HIDE"] = "True"
+
+class CustomEmbed:
+    def __init__(self, *args, **kwargs):
+        self.timestamp = kwargs.pop("timestamp", datetime.datetime.utcnow())
+        self.title = kwargs.pop("title")
+        self.description = kwargs.pop("description")
+        self.footer = kwargs.pop("footer",None)  
+        self.colour = discord.Colour.from_rgb(100, 53, 255)
+        embed = discord.Embed()
+        self.embed = embed.from_dict({"color": 6567423, "type": "rich", "title": self.title, "description": self.description, "footer": self.footer})
+ 
+    async def __aexit__(self, *args, **kwargs):
+        return self                                                                                                  
+    async def __aenter__(self):
+        return self
+        
+    async def send(self, channel, *args, **kwargs):
+        return await channel.send(embed=self.embed, *args, **kwargs)
+
+async def get_prefix(bot, message):
+    if not message.guild:
+        return commands.when_mentioned_or("oahx ")(bot, message)
+    try:
+        prefix = await bot.db.fetch("SELECT prefix FROM prefixes WHERE guild_id = $1", message.guild.id)   
+        prefix = prefix['prefix']
+        return commands.when_mentioned_or(prefix)(bot, message)
+    except Exception:
+        return commands.when_mentioned_or("oahx ")(bot, message)
+        
+class Oahx(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(allowed_mentions=discord.AllowedMentions(roles=False, users=False, replied_user=False), case_insensitive=True, *args, **kwargs)
+        self.__extensions = [f"extensions.{item[:-3]}" for item in os.listdir("/storage/emulated/0/Oahx/extensions") if item != "__pycache__"] + ["jishaku"]
+        [self.load_extension(cog) for cog in self.__extensions if cog != "__pycache__"]
+        self.colour = discord.Colour.from_rgb(100, 53, 255)
+        self.embed = CustomEmbed
+                          
+    async def on_ready(self):
+		    
+		    print(
+        "Logged in! \n"
+        f"{'-' * 20}\n"
+        f"Bot Name: {self.user} \n"
+        f"Bot ID: {self.user.id} \n"
+        f"Bot Guilds: {self.guilds} \n"
+        f"{'-' * 20}"
+    )
+
+async def run(bot):
+    bot.db = await asyncpg.create_pool("postgres://jmwizyznmjwjjv:9c8ccd9ab90e06bb398c4a6e897951e6ff401beb3d8f8f24f82658064551c8fb@ec2-52-7-168-69.compute-1.amazonaws.com:5432/d4vp6kug2vm5t2")
+    await bot.db.execute("CREATE TABLE IF NOT EXISTS prefixes(guild_id bigint PRIMARY KEY, prefix TEXT)")
+    await bot.db.execute("CREATE TABLE IF NOT EXISTS numbers(number TEXT PRIMARY KEY, channel_id bigint, name TEXT)")
+
+bot = Oahx(command_prefix=get_prefix)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run(bot=bot))
+bot.run("ODQ0MjEzOTkyOTU1NzA3NDUy.YKPJjA.zlx8JnZ3D8sftVBTXk8YwG88--I")            
---
+++ extensions/contacts.py
@@ -0,0 +1,96 @@
+import discord
+from discord.ext import commands
+from difflib import get_close_matches
+import random
+import traceback
+import asyncio
+
+class Contacts(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    async def try_channel(self, channel):
+        channel_data = self.bot.get_channel(channel)
+        if channel_data is None:
+            channel_data = await self.bot.fetch_channel(channel)
+        return channel_data
+        
+    @commands.group(name="phone", brief="A dummy command for the commands.", invoke_without_command=True)
+    async def phone(self, ctx):
+        me = await self.bot.db.fetchrow("SELECT * FROM numbers WHERE name = '%s'" % ctx.author.name)  
+        async with self.bot.embed(title="Phone", description="Your phone number is `%s`" % me["number"]) as embed:
+            await embed.send(ctx.channel)      
+        
+    @phone.command(name="call", brief="Call soemone by their phone number!")   
+    async def call(self, ctx, number : str):
+        if number == "991":
+            channel = await self.try_channel(817471364302110731)
+            def check(m):
+                roles = [r.name for r in m.author.roles]
+                return m.author.name == ctx.author.name or m.channel.id == 817471364302110731
+            await channel.send("Soemone is asking for help, please respond.")
+            await ctx.send("Please tell us your question, problem.")
+            while True:                                        
+                message = await self.bot.wait_for("message", check=check)  
+            
+                roles = [r.name for r in message.author.roles]            
+                if message.content == "cancel":
+                    await ctx.send("Call ended")    
+                    await channel.send("Call ended") 
+                    return
+                elif message.author.name == ctx.author.name and message.channel.id != 817471364302110731:
+                    await channel.send(f"{ctx.author.name}: {message.content}")
+                elif message.channel.id == 817471364302110731:
+                    await ctx.send(f"Support Team: {message.content}")
+        try:
+            phone_data = await self.bot.db.fetchrow("SELECT * FROM numbers WHERE number = '%s'" % number)
+            me = await self.bot.db.fetchrow("SELECT * FROM numbers WHERE name = '%s'" % ctx.author.name)
+        except Exception:
+            traceback.print_exc()
+            async with self.bot.embed(title="Number Not Found", description="The phone number that was provided is not correct or you do not have one.") as embed:
+                return await embed.send(ctx.channel)
+        else:
+            channel_data = await self.try_channel(phone_data['channel_id'])
+            me_channel_data = await self.try_channel(me['channel_id'])
+            async with self.bot.embed(title="Calling..", description="Calling phone number `%s`" % phone_data['number']) as embed:
+                await embed.send(ctx.channel)
+            async with self.bot.embed(title="Soemone is calling..",description=f"`{me['name']}` is calling with the phone number `{me['number']}`, do you want to pick up? [yes - no]") as embed:               
+                await embed.send(channel_data)      
+            def check(m):
+                return m.author.name == phone_data['name']  
+            try:
+                message = await self.bot.wait_for("message", timeout=30.0, check=check)
+                message = message.content.lower()
+                if message == "yes":
+                    await channel_data.send("You are now talking with `{}`".format(me["name"]))
+                    await me_channel_data.send("You are now talking with `{}`".format(phone_data["name"]))
+                    while True:
+                        def check(m):
+                            return m.author.name == phone_data["name"] or m.author.name == me["name"] and m.channel.id == phone_data["channel_id"] or m.channel.id == me["channel_id"]
+                        message = await self.bot.wait_for("message",check=check)
+                        if message.content == "cancel":
+                            await me_channel_data.send("Call ended")
+                            await channel_data.send("Call ended")
+                            break
+                            return                        
+                        elif message.author.name == phone_data["name"]:
+                            await me_channel_data.send(f"{phone_data['name']}: {message.content}")
+                        elif message.author.name == me["name"]:
+                                await channel_data.send(f"{me['name']}: {message.content}") 
+                else:
+                    await ctx.send("Did not answer")                                             
+            except asyncio.TimeoutError:
+                async with self.bot.embed(title="Call ended..", description="The call ended because no one responded..") as embed:
+                    await embed.send(ctx.channel)                                                        
+    @phone.command(name="create", brief="Create a phone number!")
+    async def create(self, ctx):
+        full_number = "0487"
+        num_ber = 123456789087681083919371037197
+        for i in range(7):
+            full_number += random.choice(str(num_ber))       
+        await self.bot.db.execute("INSERT INTO numbers(number, channel_id, name) VALUES ($1, $2, $3)", full_number, ctx.channel.id, ctx.author.name)
+        async with self.bot.embed(title="Success!", description="The operation was a success, your phone number is `%s`" % full_number) as embed:
+            return await embed.send(ctx)
+            
+def setup(bot):
+    bot.add_cog(Contacts(bot))       
