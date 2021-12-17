c515f188655648ba8d6ee520c49f758ca7b165d0
FrostiiWeeb <73381596+FrostiiWeeb@users.noreply.github.com>
2021-12-17 18:24:47 +0100

    Update CustomContext.py

--- utils/CustomContext.py
+++ utils/CustomContext.py
@@ -6,7 +6,8 @@ class CoolContext(commands.Context):
         super().__init__(*args, **kwargs)
         
     async def fancy_send(self, text : str, speed : int = 1, *args, **kwargs):
-        full_text = f"{text[0]}"                    
+        full_text = f"{text[0]}" 
+        await asyncio.sleep(0.5)
         message = await super().send(full_text, *args, **kwargs)        
         for letter in text[1:]:
             full_text += letter 
