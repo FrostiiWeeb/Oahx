0ef948ef4be4dbf0c4336105a3aef33598d86856
FrostiiWeeb <73381596+FrostiiWeeb@users.noreply.github.com>
2021-12-17 17:27:44 +0100

    Update help.py

--- help.py
+++ help.py
@@ -1,42 +1,7 @@
 import discord
 from discord.ext import commands
 from utils.paginator import OahxPaginator
-
-<<<<<<< HEAD
-        for cog, commands in mapping.items():
-           filtered = await self.filter_commands(commands, sort=True)
-           command_signatures = [self.get_command_signature(c) for c in filtered]
-           if command_signatures:
-                cog_name = getattr(cog, "qualified_name", "No Category")
-                paginator.pages.append(discord.Embed(title=cog_name, description="\n".join(command_signatures), colour=self.context.bot.colour))
-
-mappingproxy({'CustomDebugCog': <extensions.debug_command.CustomDebugCog object at 0xae3d2a00>, 'Misc': <extensions.misc.Misc object at 0xadf435b0>, 'Information': <extensions.information.Information object at 0xadf43580>, 'Error': <extensions.errors.Error object at 0xadf55238>, 'Owner': <extensions.owner.Owner object at 0xadef9940>, 'Help': <extensions.help.Help object at 0xa75ed970>, 'Contacts': <extensions.contacts.Contacts object at 0xa770d088>})
-
-class Dropdown(discord.ui.Select):
-    def __init__(self, ctx):
-        self.context = ctx
-        options = [
-            discord.SelectOption(label='Misc', description='All the misc commands.'),
-            discord.SelectOption(label='Information', description='All the commands for bot info.'),
-            discord.SelectOption(label='Owner', description='All the commands for the owner.')
-            discord.SelectOption(label="Owner", description='All the commands for contacts.')
-        ]
-        super().__init__(placeholder='Help', min_values=1, max_values=1, options=options)
-
-    async def callback(self, interaction: discord.Interaction):
-        cog = self.context.bot.get_cog(self.values[0])
-        commands = []
-        for 
-        pages = [discord.Embed(title=self.values[0], description=, colour=self.context.bot.colour)]
-        await interaction.response.edit_message(view=self, f'Your favourite colour is {self.values[0]}')
-
-
-class DropdownView(discord.ui.View):
-    def __init__(self, ctx):
-        super().__init__()
-
-        self.add_item(Dropdown(ctx))
-=======
+	
 class Trash(discord.ui.Button):
 	def __init__(self, ctx, help):
 		self.context = ctx
@@ -115,7 +80,6 @@ class DropdownView(discord.ui.View):
     async def on_timeout(self):
     	await self.message.edit('This help menu has expired', view=None, embed=None)
     	self.remove_item(Dropdown(self.context, self.h))                          
->>>>>>> refs/remotes/Oahx/master
 
 class MyHelpCommand(commands.HelpCommand):
    def get_command_signature(self, command, group_main=None):
@@ -125,20 +89,9 @@ class MyHelpCommand(commands.HelpCommand):
             return '%s%s %s' % (self.context.clean_prefix, command.qualified_name, command.signature)
     
    async def send_bot_help(self, mapping):
-<<<<<<< HEAD
-        paginator = OahxPaginator()
-        paginator.pages = []       
-
-        await paginator.paginate(self.context)           
-=======
    	view = DropdownView(ctx=self.context, help=self)
    	embedd = discord.Embed(title="Help", description="```\n[] -> optional\n<> -> required```", colour=self.context.bot.colour)
    	view.message = await self.get_destination().send(embed=embedd, view=view)
-#        paginator = OahxPaginator()
-#        paginator.pages = []       
-
-#        await paginator.paginate(self.context)           
->>>>>>> refs/remotes/Oahx/master
    
    async def send_cog_help(self, cog):
         embed = discord.Embed(title=cog.qualified_name, colour=self.context.bot.colour)
