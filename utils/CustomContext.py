31202250cff55cab4f16784ff6633c9799d98939
FrostiiWeeb <73381596+FrostiiWeeb@users.noreply.github.com>
2021-12-17 19:26:20 +0100

    Update CustomContext.py

--- utils/CustomContext.py
+++ utils/CustomContext.py
@@ -5,7 +5,6 @@ import aiohttp
 class CoolContext(commands.Context):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.bot.session = aiohttp.ClientSession()
         
     async def fancy_send(self, text : str, speed : int = 1, *args, **kwargs):
         full_text = f"{text[0]}" 
