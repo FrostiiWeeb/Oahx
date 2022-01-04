da669bac38dabefab7102f1c0c5eeb390653bd2c
FrostiiWeeb <rayanhassansuja@gmail.com>
2022-01-04 14:28:16 +0100

    Db

--- bot.py
+++ bot.py
@@ -12,8 +12,8 @@ os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
 os.environ["JISHAKU_HIDE"] = "True"
 
 async def run():
-    bot = Oahx(command_prefix=get_prefix, intents=discord.Intents.all())  
-    bot.db = await asyncpg.create_pool("postgres://jmwizyznmjwjjv:9c8ccd9ab90e06bb398c4a6e897951e6ff401beb3d8f8f24f82658064551c8fb@ec2-52-7-168-69.compute-1.amazonaws.com:5432/d4vp6kug2vm5t2")
+    db = await asyncpg.create_pool("postgres://jmwizyznmjwjjv:9c8ccd9ab90e06bb398c4a6e897951e6ff401beb3d8f8f24f82658064551c8fb@ec2-52-7-168-69.compute-1.amazonaws.com:5432/d4vp6kug2vm5t2")
+    bot = Oahx(command_prefix=get_prefix, intents=discord.Intents.all(), db=db)  
     await bot.db.execute("CREATE TABLE IF NOT EXISTS prefixes(guild_id bigint PRIMARY KEY, prefix TEXT)")  
     await bot.db.execute("CREATE TABLE IF NOT EXISTS numbers(number TEXT PRIMARY KEY, channel_id bigint, name TEXT)")
     await bot.db.execute("CREATE TABLE IF NOT EXISTS premium_users(code TEXT PRIMARY KEY, user_id bigint, name TEXT)")
@@ -41,7 +41,8 @@ class Oahx(commands.AutoShardedBot):
     def __init__(self, *args, **kwargs):
         super().__init__(allowed_mentions=discord.AllowedMentions(roles=False, users=False, replied_user=False), case_insensitive=True, *args, **kwargs)
         self.__extensions = [f"extensions.{item[:-3]}" for item in os.listdir("./extensions")]
-        self.help_command = None        
+        self.help_command = None  
+        self.db = kwargs.pop("db", None)      
         self.colour = discord.Colour.from_rgb(100, 53, 255)
         self.maintenance = False
         self.owner_maintenance = False
