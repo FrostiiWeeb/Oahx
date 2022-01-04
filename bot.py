b74cf16082fabe5f006d0ed6adbc243fe15e39c7
FrostiiWeeb <rayanhassansuja@gmail.com>
2022-01-04 17:18:32 +0100

    FIX DB

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
 os.environ["JISHAKU_HIDE"] = "True"
 
 async def run():
-    db = await asyncpg.create_pool("postgres://jmwizyznmjwjjv:9c8ccd9ab90e06bb398c4a6e897951e6ff401beb3d8f8f24f82658064551c8fb@ec2-52-7-168-69.compute-1.amazonaws.com:5432/d4vp6kug2vm5t2")
+    db = await asyncpg.connect('postgresql://postgres@localhost/alex')
     bot = Oahx(command_prefix=get_prefix, intents=discord.Intents.all(), db=db)  
     await bot.db.execute("CREATE TABLE IF NOT EXISTS prefixes(guild_id bigint PRIMARY KEY, prefix TEXT)")  
     await bot.db.execute("CREATE TABLE IF NOT EXISTS numbers(number TEXT PRIMARY KEY, channel_id bigint, name TEXT)")
