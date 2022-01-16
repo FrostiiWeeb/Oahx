1586ae2c8dfce60af9b57c1080f8ef36ea4ecc8d
FrostiiWeeb <rayanhassansuja@gmail.com>
2022-01-16 13:42:51 +0100

    ipc routes

--- extensions/ipc.py
+++ extensions/ipc.py
@@ -9,6 +9,14 @@ class IpcRoutes(commands.Cog):
     async def get_guild_count(self, data):
         return len(self.bot.guilds)
 
+    @ipc.server.route()
+    async def get_channel_count(self, data):
+        return len([channel for channel in self.bot.get_all_channels()])
+
+    @ipc.server.route()
+    async def get_user_count(self, data):
+        return len(self.bot.users)
+
 
 def setup(bot):
     bot.add_cog(IpcRoutes(bot))
