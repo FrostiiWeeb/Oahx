da88bfe077d3bddd8fd44bcb53ead239ae990871
FrostiiWeeb <rayanhassansuja@gmail.com>
2022-01-28 13:07:11 +0100

    Fix some break/return stuff

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ async def run():
     bot.db = await asyncpg.create_pool(
         dsn="postgresql://frostiiweeb:my_password@localhost/oahx", max_queries=100000000
     )
-    await bot.cogs["Music"].load()
+    await bot.cogs["Music"].start_nodes()
     redis = await asyncrd.connect("redis://localhost:7000")
     bot.redis = redis
     await bot.db.execute(
