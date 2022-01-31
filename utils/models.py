e0ba112d9c165f4f1da8a2ee9b3c3117e4b339e3
FrostiiWeeb <rayanhassansuja@gmail.com>
2022-01-31 21:50:50 +0100

    Fix some break/return stuff

--- utils/models.py
+++ utils/models.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import typing, discord, asyncio, itertools, functools, random
 from discord.ext import commands
 import youtube_dl
@@ -66,8 +67,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWAREdiscord.
 """
-
-from __future__ import annotations
 
 import asyncio
 import logging
