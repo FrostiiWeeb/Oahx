7f618e7b00b502a6ffaa6aa2f3ac2a8be9d4d525
FrostiiWeeb <rayanhassansuja@gmail.com>
2022-01-31 18:00:53 +0100

    Fix some break/return stuff

--- bot.py
+++ bot.py
@@ -290,6 +290,7 @@ class Oahx(commands.AutoShardedBot):
                     return await self.invoke(context)
                 return await self.process_commands(message)
         else:
+            print("yes")
             return self.mounts[int(whichbot["bot"])-1].dispatch("message", message=message)
 
     async def on_message_edit(self, before: discord.Message, after: discord.Message):
