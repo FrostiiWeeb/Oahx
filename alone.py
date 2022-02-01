688cc3bf686bbf91bdec8435bbd8079b36c625e5
FrostiiWeeb <rayanhassansuja@gmail.com>
2022-02-01 16:26:34 +0100

    Fix

--- alone.py
+++ alone.py
@@ -10,6 +10,10 @@ class Alone(commands.Bot):
 	def __init__(self, command_prefix, help_command=None, description=None, mount = None, **options):
 		super().__init__(command_prefix, help_command, description, **options)
 		self._bot : commands.Bot = mount
+
+	@property
+	def user(self):
+		return self._bot._connection.user
 		
 	async def get_context(self, message: discord.Message, *, cls = CoolContext):
 		"""|coro|
