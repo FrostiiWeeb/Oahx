d8d789cec33d4f3b1d3e6c1396f518e87651c762
FrostiiWeeb <rayanhassansuja@gmail.com>
2022-03-04 21:41:02 +0100

    🚀 rewrite

--- utils/modal.py
+++ utils/modal.py
@@ -1,29 +1,21 @@
-import discord
-from typing import *
-from discord.ext import commands
-import secrets
 import asyncio
+import uuid
+import json
 
-"""
-{
-  "title": "My Cool Modal",
-  "custom_id": "cool_modal",
-  "components": [{
-    "type": 1,
-    "components": [{
-      "type": 4,
-      "custom_id": "name",
-      "label": "Name",
-      "style": 1,
-      "min_length": 1,
-      "max_length": 4000,
-      "placeholder": "John",
-      "required": true
-    }]
-  }]
-}
-"""
+from enum import Enum
+from typing import *
 
+import discord
+
+from discord.ext import commands
+from discord.http import Route
+
+R = TypeVar("R", bound="Response")
+V = TypeVar("V", bound="InputValue")
+class Response(Generic[R]):
+    def __init__(self, interaction: discord.Interaction, responses: List[V]) -> None:
+        self.interaction = interaction
+        self.responses = responses
 
 class InputValue:
     def __init__(self, base, value: str) -> None:
@@ -31,20 +23,15 @@ class InputValue:
         self.value = value
 
 
-class Style:
-    @staticmethod
-    def singleline():
-        return 1
-
-    @classmethod
-    def multiline():
-        return 2
+class Style(Enum):
+    singleline = 1
+    multiline = 2
 
 
 class Input:
     def __init__(self, payload: dict) -> None:
         self.type = payload["type"]
-        self.custom_id = secrets.token_urlsafe(16)
+        self.custom_id = str(uuid.uuid4())
         self.style = payload["style"]
         self.label = payload["label"]
         self.min_length = payload.get("min_length")
@@ -59,13 +46,12 @@ class Modal:
         self.title = title
         self.bot = bot
         self.input_fields = []
-        self.custom_id = secrets.token_urlsafe(16)
-        self.data = {"title": title, "custom_id": self.custom_id, "components": []}
-        self.sender = discord.webhook.async_.async_context.get()
+        self.custom_id = str(uuid.uuid4())
+        self.data = {"type": 9, "title": title, "custom_id": self.custom_id, "components": []}
         self.payload = []
         self.responded = False
 
-    def input(
+    def add_input(
         self,
         type: Style,
         label: str,
@@ -77,7 +63,7 @@ class Modal:
     ):
         component = {
             "type": 4,
-            "custom_id": secrets.token_urlsafe(16),
+            "custom_id": str(uuid.uuid4()),
             "style": type,
             "label": label,
             "required": str(required),
@@ -96,31 +82,31 @@ class Modal:
 
     async def send(self, interaction: discord.Interaction):
         self.responded = True
-        await self.sender.create_interaction_response(
-            interaction_id=interaction.id, token=interaction.token, session=interaction._session, data=self.data, type=9
-        )
+        route = Route("POST", f"/interactions/{interaction.id}/{interaction.token}/callback")
+        await self.bot.http.request(route, headers={"Content-Type": "application/json"}, json=self.data)
 
     @property
     def sent(self):
         return self.responded
 
-    async def get_value(self, wait_before_exiting: int = 180):
-        def check(interaction):
+    async def get_value(self, timeout: int = 180):
+        def check(interaction: discord.Interaction):
             return interaction.data.get("custom_id") == self.custom_id
 
         try:
-            msg: dict = await self.bot.wait_for("socket_raw_receive", check=check, timeout=wait_before_exiting)
+            msg: str = await self.bot.wait_for("socket_raw_receive", check=check, timeout=timeout)
         except asyncio.TimeoutError:
             return None, []
-        int = discord.Interaction(state=(self.bot._get_state()), data=msg)
+        msg: Dict[str, Any] = json.loads(msg)
+        int = discord.Interaction(state=(self.bot._connection), data=msg)
         if msg.get("t") == "MODAL_SUMBIT":
             data = msg.get("data")
         components = data["components"]
         result = []
         for component in components:
-            for field in self.fields:
+            for field in self.input_fields:
                 if component["components"][0]["custom_id"] == field.custom_id:
                     field.value = component["components"][0]["value"]
-                    result.append(field)
+                    result.append(InputValue(field, field.value))
 
-        return int, result
+        return Response(int, result)
