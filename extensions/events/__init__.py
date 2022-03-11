d28e1be484db9b7a588e60e137a7c3bcc34a69fe
FrostiiWeeb <rayanhassansuja@gmail.com>
2022-03-11 15:31:25 +0100

    Re-formatted

--- extensions/events/__init__.py
+++ extensions/events/__init__.py
@@ -2,7 +2,7 @@ from ._emoji import *
 from ._errors import *
 from ._command import *
 
-class Events(Emoji, Error, Completion):
+class Events(Emoji, Completion, Error):
 	def __init__(self, bot):
 		super().__init__(bot)
 
